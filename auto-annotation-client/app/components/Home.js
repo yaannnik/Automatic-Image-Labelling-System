e6cd373aa953ee8e39655cda723546f7958275d0
jing peng <joanna@jings-Air.fios-router.home>
2021-12-02 13:51:08 -0500

    change ui format

--- auto-annotation-client/app/components/Home.js
+++ auto-annotation-client/app/components/Home.js
@@ -1,33 +1,33 @@
-
 /* eslint-disable import/no-unresolved */
 
 // component ui framework
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import React from "react";
+import { Grid } from "semantic-ui-react";
 // internal component
 
-import ImageDisplay from '../features/imageDisplay/imageDisplay';
-import ImageHistory from '../features/imageHistory/imageHistory';
+import ImageDisplay from "../features/imageDisplay/imageDisplay";
+import ImageHistory from "../features/imageHistory/imageHistory";
 
 // data structure
 
-import AppData from '../dataStructure/AppData';
+import AppData from "../dataStructure/AppData";
 
 export default function imageHome() {
   const AppDataInfo = [];
 
   return (
-    <Grid columns={2} divided>
-      <Grid.Column computer={8} largeScreen={10} widescreen={12}>
-        <div className=".app-info-panel" data-tid="appinfo">
-          <ImageDisplay
-            imgData={AppDataInfo}
-          />
-        </div>
-      </Grid.Column>
-      <Grid.Column computer={8} largeScreen={6} widescreen={4}>
-        <ImageHistory />
-      </Grid.Column>
-    </Grid>
+    <div>
+      <br />
+      <Grid columns={2} divided>
+        <Grid.Column computer={12} largeScreen={12} widescreen={12}>
+          <div className=".app-info-panel" data-tid="appinfo">
+            <ImageDisplay imgData={AppDataInfo} />
+          </div>
+        </Grid.Column>
+        <Grid.Column computer={4} largeScreen={4} widescreen={4}>
+          <ImageHistory />
+        </Grid.Column>
+      </Grid>
+    </div>
   );
 }
