814f6d77f74223673983f84829d23434805a4e4a
jing peng <joanna@jings-Air.fios-router.home>
2021-12-04 10:55:04 -0500

    change ui in add annotation panel

--- auto-annotation-client/app/features/imageOperations/AddAnnotation.jsx
+++ auto-annotation-client/app/features/imageOperations/AddAnnotation.jsx
@@ -60,7 +60,9 @@ export default function AddAnnotation(props: {
   return (
     <Grid padded="vertically">
       <Grid.Row>
-        <h1>Add annotation</h1>
+        <h1>Add an annotation</h1>
+      </Grid.Row>
+      <Grid.Row>
         <Label size="large" color="green">Choose a category: mask/unmask</Label>
       </Grid.Row>
       <Grid.Row>
