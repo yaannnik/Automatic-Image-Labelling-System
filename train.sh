9417c569c213a40353f3d260ad3eb68dd76862de
yaannnik <yaannnikk@gmail.com>
2021-12-20 01:34:08 -0500

    train.sh delete conda env
    Signed-off-by: yaannnik <yaannnikk@gmail.com>

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-conda activate pytorch
+# conda activate pytorch
 cd auto-annotation-server/detectron2/tools
 python train_net.py --config-file ../configs/COCO-Detection/faster_rcnn_R_34_FPN_3x.yaml
